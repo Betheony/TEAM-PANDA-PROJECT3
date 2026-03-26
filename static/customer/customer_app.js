e7bb5d36dd3e23ef9a6473a1c86db17e3e85e61a
ZerithGames <106826422+ZerithGames@users.noreply.github.com>
2026-03-26 14:07:12 -0500

    Changed the cursor image. Added a selection effect to the menu items. Made it so that menu item names are displayed in the cart instead of IDs.

--- static/customer/customer_app.js
+++ static/customer/customer_app.js
@@ -4,32 +4,53 @@
 *   This needs a better description :(
 */
 
-// This needs to be an array of integer order IDs that contains what the customer has in their "cart".
+// This needs to be an array of string IDs that contains what the customer has in their "cart".
 // Anything in this array will be sent to the database to order.
 // An array is used to make it easy to display all the items.
+// When the array is sent to the DB, the entries need to be converted to strings.
 
-let customer_items = []
+let customer_cart = []
 
+let menu_id_to_name_struct = {
+
+    "101": "Ancestral Samurai Sakura Tea",
+    "102": "Iced Yurei Matcha",
+    "103": "Patriotic Cold Brew",
+    "104": "Bethany's Ballistic Boba Bash",
+    "105": "Dr. Taele's Signature Smoothie"
+}
+
+// This is a function that adds an item to a cart.
 function add_item_to_cart( _item ) {
 
-    try {
+    customer_cart.push( parseInt( _item ) );
+    show_cart_contents();
+}
 
-        customer_items.push( parseInt( _item ) );
+// Function to update the cart contents, which are shown on-screen.
+// TODO: Make it so that group counts are displayed, not individual items.
+// Ex. Have "2x Cold Brew" instead of 2 separate "Cold Brew" listings.
+function show_cart_contents() {
 
-        console.log(_item);
-        console.log(customer_items);
+    var _innerHTMLstring = "";
+    for( var _i = 0; _i < customer_cart.length; _i++ ) {
 
-        var _innerHTMLstring = "";
-        for( var _i = 0; _i < customer_items.length; _i++ ) {
-
-            _innerHTMLstring = _innerHTMLstring + "<p>" + customer_items.at(_i) + "</p>";
-        }
-
-        document.getElementById("cart_items").innerHTML = _innerHTMLstring;
+        _innerHTMLstring = _innerHTMLstring + "<p>" + menu_id_to_name_struct[ customer_cart.at(_i) ] + "</p>";
     }
-    catch( _error ) {
 
-        console.log(_error)
-        return;
+    document.getElementById("cart_items").innerHTML = _innerHTMLstring;
+}
+
+// This function deletes everything in the cart array and then displays the now empty cart.
+function clear_cart() {
+
+    for( var i = 0; i < customer_cart.length; i++ ) {
+
+        delete customer_cart[i];
     }
+
+    customer_cart = []
+    customer_cart.length = 0;
+
+    show_cart_contents();
 }
