3ab9bebb91a2cd1325e32b61583c56f8f2fbbe09
ZerithGames <106826422+ZerithGames@users.noreply.github.com>
2026-03-26 13:31:25 -0500

    I added a rudimentary cart system.

--- static/customer/customer_app.js
+++ static/customer/customer_app.js
@@ -3,7 +3,33 @@
 *   This is the app to control the customer interface...
 *   This needs a better description :(
 */
-function place_order_test() {
 
-    console.log("hi");
+// This needs to be an array of integer order IDs that contains what the customer has in their "cart".
+// Anything in this array will be sent to the database to order.
+// An array is used to make it easy to display all the items.
+
+let customer_items = []
+
+function add_item_to_cart( _item ) {
+
+    try {
+
+        customer_items.push( parseInt( _item ) );
+
+        console.log(_item);
+        console.log(customer_items);
+
+        var _innerHTMLstring = "";
+        for( var _i = 0; _i < customer_items.length; _i++ ) {
+
+            _innerHTMLstring = _innerHTMLstring + "<p>" + customer_items.at(_i) + "</p>";
+        }
+
+        document.getElementById("cart_items").innerHTML = _innerHTMLstring;
+    }
+    catch( _error ) {
+
+        console.log(_error)
+        return;
+    }
 }
